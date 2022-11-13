75956b1a149082c6dc3a0a6d6911ea60bbc03a3e
RenGVZ <gaving@live.com>
2022-11-13 19:09:00 +0900

    added main components and rendered cryptocurrencies

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components'
+import './index.css';
 
 const App = () => {
   return (
     <div className="app">
       <div className="navbar">
-      
+        <Navbar />
       </div>
       <div className="main">
-
-      </div>
-      <div className="footer">
-
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={ <Homepage />} />
+              <Route exact path="/exchanges" element={ <Exchanges />} />
+              <Route exact path="/cryptocurrencies" element={ <Cryptocurrencies />} />
+              <Route exact path="/crypto/:coinId" element={ <CryptoDetails />} />
+              <Route exact path="/news" element={ <News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}>
+            Cryptocloud <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+import icon from '../images/cryptoicon.png'
 
 const Navbar = () => {
   return (
     <div className="nav-container">
-      <div className="logo-container">
+      <div className="logo-container" style={{flexDirection: 'column'}}>
         <Avatar src={icon} size="large"/>
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptocloud</Link>
@@ -14,6 +15,20 @@ const Navbar = () => {
         {/* <Button className="menu-control-container">
 
         </Button> */}
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />} key="one">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />} key="two">
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />} key="three">
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />} key="four">
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
       </div>
     </div>
   )
